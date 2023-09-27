e879ce0fadd1513cab1aed63bb2c070f5b238f46
DevStylish <moisesmarper10@gmail.com>
2023-09-27 22:15:46 +0200

    Primer commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Creando codigo con Moises';
+export const SITE_DESCRIPTION = 'Bienvenid@ a mi web, esta es una página que usaré como blog y portfolio (además de incluir alguna que otro post friki)';
